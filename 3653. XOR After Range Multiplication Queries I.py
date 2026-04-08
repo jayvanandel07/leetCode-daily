5a8b3237d241a2329c34ea793cf24d20655bdd69
Jayvan Andel S <75113307+jayvanandel07@users.noreply.github.com>
2026-04-08 07:20:14 +0000

    3653 pass 2

--- 3653. XOR After Range Multiplication Queries I.py
+++ 3653. XOR After Range Multiplication Queries I.py
@@ -1,16 +1,12 @@
 class Solution:
-    def bitwiseXor(self, nums: List[int], i:int=0 )-> int:
-        if i+1 == len(nums):
-            return nums[i]^0
-        return nums[i]^self.bitwiseXor(nums,i+1)
 
     def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        MOD= 10**9 + 7
         for (l,r,k,v) in queries:
-            idx=l
-            while idx<=r:
-                nums[idx] = (nums[idx] * v) % (10**9 + 7)
-                idx += k
-        
-        return self.bitwiseXor(nums) 
-        
-
+            for i in range(l, r + 1, k):
+                nums[i] = (nums[i] * v) % MOD
+                
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
